6cb9c52cbdb59fabcfbdce940b95211d02280a49
Desk6661 <sadhudip212@gmail.com>
2025-04-20 16:44:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    let items = ["rock", "paper", "scissor"];
+    let randomNum = Math.floor((Math.random() * 3));
+    return items[randomNum];
+}
+
+function getHumanChoice(){
+    return prompt("enter rock, paper or scissor", "");
+}
+
+function playRound(humanChoice,ComputerChoice){
+    humanChoice.toLowerCase();
+
+    if (humanChoice === "rock" && ComputerChoice === "scissor") {
+        console.log("computer lose!, human wins!");
+        humanScore++;
+    } else if(humanChoice === "paper" && ComputerChoice === "rock"){
+        console.log("computer lose!, human wins!");
+        humanScore++;
+    } else if(humanChoice === "scissor" && ComputerChoice === "paper"){
+        console.log("computer lose!, human wins!");
+        humanScore++;
+    } else if(humanChoice === ComputerChoice){
+        console.log("tie! no one wins!");
+    } else {
+        console.log("humans lose! computer wins!");
+        computerScore++;
+    }
+}
+
+function playGame(){
+    let exit = 100;
+
+    for(let i = 0; i < exit; i++){
+        const humanChoice = getHumanChoice();
+        const ComputerChoice = getComputerChoice();
+
+        console.log("human choice :", humanChoice);
+        console.log("computer choice :", ComputerChoice);
+
+        playRound(humanChoice, ComputerChoice);
+
+        if (humanScore === 5) {
+            exit = 0;
+            return console.log("human wins the competition with 5 points"); 
+        } else if (computerScore === 5){
+            exit = 0;
+            return console.log("computer wins the competition with 5 points"); 
+        }
+    }
+}
+
+playGame();
